8bab0db0dd70f59b26ee9597b73db2adc2709f78
janiskaz <janikazo@madona.edu.lv>
2025-11-10 12:24:57 +0200

    Atrisinu 1. uzdevumu

--- 1uzdevums.py
+++ 1uzdevums.py
@@ -4,3 +4,17 @@ Summējot divus norādītos veselos skaitļus.
 Tomēr, ja summa ir no 15 līdz 20, tā atgriezīs 20.
 
 """
+def spec_summa(a, b): # a un b ir parametri
+    kopaa=a+b
+    if 15<=kopaa<=20:
+        return 20
+    else:
+        return kopaa
+    
+# piemērs
+skaitlis1=int(input("Ievadi pirmo skaitli: "))
+skaitlis2=int(input("Ievadi otro skaitli: "))
+
+rezultats=spec_summa(skaitlis1, skaitlis2) # jānorāda divi argumenti
+
+print(f"Rezultāts ir: {rezultats}")
