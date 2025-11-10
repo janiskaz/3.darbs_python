1635a61239edbd51a44cd4b5258a07bcf01d1a90
janiskaz <janikazo@madona.edu.lv>
2025-11-10 12:54:11 +0200

    Atrisinu 3. uzdevumu

--- 3uzdevums.py
+++ 3uzdevums.py
@@ -4,3 +4,13 @@ pirmās un pēdējās krāsas no šī saraksta.
 
 krasa_saraksts = ["sarkans", "zaļš", "balts", "melns"]
 """
+
+krasa_saraksts = ["sarkans", "zaļš", "balts", "melns"] # saraksts/masīvs
+
+# pirmo krāsu
+pirma_krasa = krasa_saraksts[0]
+print(f"Pirmā krāsa sarakstā ir: {pirma_krasa}")
+
+# pēdējo krāsu
+pedeja_krasa = krasa_saraksts[-1]
+print(f"Pēdējais elements ir: {pedeja_krasa}")
